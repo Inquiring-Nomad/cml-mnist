d6c2dca20603ea7a4258c7319f20faee1c489469
Akis Loumpourdis <akis@akiss-air.home>
2021-06-03 21:46:57 +0100

    Requirements

---
+++ train.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+# Set random seed
+seed = 42
