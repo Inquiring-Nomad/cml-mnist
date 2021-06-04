d24a4ffcc3cdf7571a9f0f6dc2a87b4f2434a3c1
Akis Loumpourdis <akis@akiss-air.home>
2021-06-04 12:16:25 +0100

    Added test set metrics

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix,accuracy_score
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 from sklearn.model_selection import cross_val_score,cross_val_predict
 from sklearn.ensemble import RandomForestClassifier
@@ -23,73 +23,17 @@ train_score = cross_val_score(rf, X_train, y_train,cv=5)
 
 predictions = cross_val_predict(rf, X_train, y_train,cv=5)
 
-print(f"Mean Cross Validation Score : {100 * np.mean(train_score):.2f}")
+
 with open("metrics.txt", 'w') as outfile:
         outfile.write(f"Mean Cross Validation Score : {100 * np.mean(train_score):.2f}")
 
 skplt.metrics.plot_confusion_matrix(y_train, predictions, normalize=True)
-plt.savefig("Confusion_Matrix.png")
-
-exit()
+plt.savefig("train_confusion_matrix.png")
 
 
-# Fit a model on the train section
-# regr = RandomForestRegressor(max_depth=2, random_state=seed)
-# regr.fit(X_train, y_train)
+# Test set
+rf.fit(X_train, y_train)
+test_score = rf.score(X_test,y_test)
+with open("metrics.txt", 'a') as outfile:
+        outfile.write(f"Test Score : {100 * test_score:.2f}")
 
-# Report training set score
-train_score = regr.score(X_train, y_train) * 100
-# Report test set score
-test_score = regr.score(X_test, y_test) * 100
-
-# Write scores to a file
-with open("metrics.txt", 'w') as outfile:
-        outfile.write("Training variance explained: %2.1f%%\n" % train_score)
-        outfile.write("Test variance explained: %2.1f%%\n" % test_score)
-
-
-##########################################
-##### PLOT FEATURE IMPORTANCE ############
-##########################################
-# Calculate feature importance in random forest
-importances = regr.feature_importances_
-labels = df.columns
-feature_df = pd.DataFrame(list(zip(labels, importances)), columns = ["feature","importance"])
-feature_df = feature_df.sort_values(by='importance', ascending=False,)
-
-# image formatting
-axis_fs = 18 #fontsize
-title_fs = 22 #fontsize
-sns.set(style="whitegrid")
-
-ax = sns.barplot(x="importance", y="feature", data=feature_df)
-ax.set_xlabel('Importance',fontsize = axis_fs)
-ax.set_ylabel('Feature', fontsize = axis_fs)#ylabel
-ax.set_title('Random forest\nfeature importance', fontsize = title_fs)
-
-plt.tight_layout()
-plt.savefig("feature_importance.png",dpi=120)
-plt.close()
-
-
-##########################################
-############ PLOT RESIDUALS  #############
-##########################################
-
-y_pred = regr.predict(X_test) + np.random.normal(0,0.25,len(y_test))
-y_jitter = y_test + np.random.normal(0,0.25,len(y_test))
-res_df = pd.DataFrame(list(zip(y_jitter,y_pred)), columns = ["true","pred"])
-
-ax = sns.scatterplot(x="true", y="pred",data=res_df)
-ax.set_aspect('equal')
-ax.set_xlabel('True wine quality',fontsize = axis_fs)
-ax.set_ylabel('Predicted wine quality', fontsize = axis_fs)#ylabel
-ax.set_title('Residuals', fontsize = title_fs)
-
-# Make it pretty- square aspect ratio
-ax.plot([1, 10], [1, 10], 'black', linewidth=1)
-plt.ylim((2.5,8.5))
-plt.xlim((2.5,8.5))
-
-plt.tight_layout()
-plt.savefig("residuals.png",dpi=120)
